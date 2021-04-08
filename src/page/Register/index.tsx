0c212f4399861f1daa6b1a6aae3255b10cae2310
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-04-08 18:35:53 -0300

    feature: form and axios

--- src/page/Register/index.tsx
+++ src/page/Register/index.tsx
@@ -1,30 +1,88 @@
-import { Container, InnerContainer, SvgContainer, InfoContainer } from "./style";
+import {
+  Container,
+  InnerContainer,
+  SvgContainer,
+  InfoContainer,
+} from "./style";
 import Input from "../../components/Atoms/Input";
 import Button from "../../components/Atoms/Button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const Register = () => {
-  return <Container>
-    <InnerContainer>
+  interface IFormValue {
+    email: string;
+    password: string;
+  }
 
-     <InfoContainer>
+  const schema = yup.object().shape({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  });
 
-     <h2>Cadastrar</h2>
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm<IFormValue>({
+    resolver: yupResolver(schema),
+  });
 
-    <Input  height={3.5} placeHolder={'Email'} width={30}/>
-    <Input  height={3.5} placeHolder={'Senha'} width={30}/>
+  const onSubmit = (data: IFormValue) => {
+    reset();
 
-    <Button height={5} width={32} color={'#fff'} text={'Register'} background={'#fc923f'} click={() => console.log('oi')} />
-    <Button height={4} width={20} color={'#fff'} text={'Login'} background={'#14142b'} click={() => console.log('oi')}/>
+    console.log(data);
+  };
 
-     </InfoContainer>
-    <SvgContainer>
-      <img src='./assets/registerImage.svg' alt={''}/>
-    </SvgContainer>
+  return (
+    <Container>
+      <InnerContainer>
+        <InfoContainer>
+          <h2>Cadastrar</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              {...register("email")}
+              height={3.5}
+              placeHolder={"Email"}
+              width={30}
+            />
+            <p>{errors.email?.message}</p>
 
+            <Input
+              {...register("password")}
+              height={3.5}
+              placeHolder={"Senha"}
+              width={30}
+            />
+            <p>{errors.password?.message}</p>
 
-    </InnerContainer>
+            <Button
+              height={5}
+              width={32}
+              color={"#fff"}
+              text={"Register"}
+              background={"#fc923f"}
+              click={() => console.log("oi")}
+            />
+          </form>
 
-  </Container>;
+          <Button
+            height={4}
+            width={20}
+            color={"#fff"}
+            text={"Login"}
+            background={"#14142b"}
+            click={() => console.log("oi")}
+          />
+        </InfoContainer>
+        <SvgContainer>
+          <img src="./assets/registerImage.svg" alt={""} />
+        </SvgContainer>
+      </InnerContainer>
+    </Container>
+  );
 };
 
 export default Register;
