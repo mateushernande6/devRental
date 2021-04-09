d532bdfaa052e64c2fb13a8fdd2d45e7e1cb8d23
Nathan <nathan.ar.94@gmail.com>
2021-04-09 17:52:10 -0300

    feature: PreRegister code

--- src/page/PreRegister/index.tsx
+++ src/page/PreRegister/index.tsx
@@ -1,7 +1,41 @@
-import { Container } from "./style";
+import {
+  Card,
+  Container,
+  ContainerDiv,
+  DivCards,
+  DivItens,
+  Img,
+} from "./style";
+import image1 from "./assets/asde1.svg";
+import { BsPeopleCircle } from "react-icons/bs";
+import { AiFillBank } from "react-icons/ai";
+import { useHistory } from "react-router";
 
 const PreRegister = () => {
-  return <Container>PreRegister</Container>;
+  const history = useHistory();
+  const handleHistory = () => {
+    history.push("/register");
+  };
+  return (
+    <Container>
+      <ContainerDiv>
+        <Img src={image1} />
+        <DivItens>
+          <h1>Selecione seu perfil para acessar a devRental</h1>
+          <DivCards>
+            <Card onClick={handleHistory}>
+              <BsPeopleCircle />
+              <h2>Sou Dev</h2>
+            </Card>
+            <Card onClick={handleHistory}>
+              <AiFillBank />
+              <h2>Sou Empresa</h2>
+            </Card>
+          </DivCards>
+        </DivItens>
+      </ContainerDiv>
+    </Container>
+  );
 };
 
 export default PreRegister;
