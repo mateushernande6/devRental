fedb789b6cb9b1af13572bd864227ca4810705b2
Mateus Hernandes <mateushernandes939@gmail.com>
2021-04-12 18:08:37 -0300

    feat: created protection routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,38 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../page/Home";
 import Login from "../page/Login";
 import Register from "../page/Register";
 import PreRegister from "../page/PreRegister";
 import Dashboard from "../page/Dashboard";
 import NotFound from "../page/NotFound";
+import { ConstructorTypeNode, JsxTagNameExpression } from "typescript";
+import { ComponentElement } from "react";
+
+interface IPrevate {
+  component: any;
+  path: string;
+}
+
+const token = localStorage.getItem("token");
+
+const PrivateRoute = ({ component: Component, ...rest }: IPrevate) => (
+  <Route
+    {...rest}
+    render={(props) => (token ? <Component {...props} /> : <Redirect to="/" />)}
+  />
+);
 
 const Routes = () => {
   return (
     <Switch>
+      <PrivateRoute path="/dashboard" component={Dashboard} />
+
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/preregister" component={PreRegister} />
-      <Route exact path="/dashboard" component={Dashboard} />
-      <Route path="/notfound" component={NotFound} />
+
+      <Route component={NotFound} />
     </Switch>
   );
 };
