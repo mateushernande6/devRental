4a48fb01af60dcc342a6dce4767520438c97fbfa
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-04-13 11:28:28 -0300

    feature: page notfound style

--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,7 +1,12 @@
-import { Container } from "./style";
+import { Container, Image } from "./style";
 
 const NotFound = () => {
-  return <Container>Not Found</Container>;
+  return (
+    <Container>
+      Ooops... Essa pagina não existe.
+      <Image src="./assets/404.svg" alt="404" />
+    </Container>
+  );
 };
 
 export default NotFound;
--- src/page/NotFound/style.ts
+++ src/page/NotFound/style.ts
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  font-size: 10rem;
+  font-size: 5rem;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+
+  @media (max-width: 930px) {
+    justify-content: center;
+  }
+
+  @media (max-width: 530px) {
+    justify-content: space-around;
+    height: 70vh;
+  }
+`;
+
+export const Image = styled.img`
+  width: 600px;
+  height: auto;
+
+  @media (max-width: 930px) {
+    width: 80%;
+  }
 `;
