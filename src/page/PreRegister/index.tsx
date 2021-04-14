175647f4d5265944cf75c9d9772fc03c30bf4293
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-04-14 11:20:52 -0300

    feature: mobile page preregister

--- src/page/PreRegister/index.tsx
+++ src/page/PreRegister/index.tsx
@@ -4,10 +4,14 @@ import {
   ContainerDiv,
   DivCards,
   DivItens,
+  DivImage,
   Img,
+  DivText,
+  DivCardIcon,
 } from "./style";
 import image1 from "./assets/asde1.svg";
 import { BsPeopleCircle } from "react-icons/bs";
+import { FiUser, FiHome } from "react-icons/fi";
 import { AiFillBank } from "react-icons/ai";
 import { useContext } from "react";
 import { AuthDashboardContext } from "../../Provider/AuthDashboard";
@@ -31,16 +35,27 @@ const PreRegister = () => {
   return (
     <Container>
       <ContainerDiv>
-        <Img src={image1} />
+        <DivImage>
+          <Img src={image1} />
+        </DivImage>
         <DivItens>
-          <h1>Selecione seu perfil para acessar a devRental</h1>
+          <DivText>
+            <h1>Selecione seu perfil para acessar a devRental</h1>
+          </DivText>
           <DivCards>
             <Card onClick={() => functionDev("dev")}>
-              <BsPeopleCircle />
+              <DivCardIcon>
+                <FiUser size={50} />
+              </DivCardIcon>
+              {/* <BsPeopleCircle /> */}
               <h2>Sou Dev</h2>
             </Card>
             <Card onClick={() => functionCompany("company")}>
-              <AiFillBank />
+              <DivCardIcon>
+                <FiHome size={50} />
+              </DivCardIcon>
+
+              {/* <AiFillBank /> */}
               <h2>Sou Empresa</h2>
             </Card>
           </DivCards>
