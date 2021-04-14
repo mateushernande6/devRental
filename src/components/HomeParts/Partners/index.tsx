1b647a213ebd634a4a5d0f2ec2e2039681534cf1
Igor Ribeiro <igorflribeiro@gmail.com>
2021-04-13 23:35:45 -0300

    feature: carousel itens now set acording to screen size

--- src/components/HomeParts/Partners/index.tsx
+++ src/components/HomeParts/Partners/index.tsx
@@ -5,13 +5,50 @@ import {
 import {OuterContainer, Container} from "../style";
 
 import Carousel from "react-elastic-carousel";
+import {useEffect, useState} from "react";
+
+const useWindowSize = () => {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth])
+
+  useEffect(()=>{
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth])
+    }
+    window.addEventListener('resize', handleResize)
+
+    return()=>{
+      window.removeEventListener('resize', handleResize)
+    }
+  },[])
+  return size
+}
 
 const Partners = () => {
+  const [height ,width] = useWindowSize()
+
+  // let itemsToShow = 0
+
+  const handleCarousel = () => {
+    if (width < 321){
+      return 1
+    }
+
+    else if(width >= 321 && width < 426){
+        return 2
+    }
+    else if(width >= 426 && width < 769){
+      return 3
+    }
+    return 4
+  }
+
+
   return (
     <OuterContainer>
       <Container>
         <UpperContainer>
           <h2>Empresas Parceiras</h2>
+          <h2>{width}</h2>
 
           <p>lorem ipsum dolor sit amet </p>
         </UpperContainer>
@@ -20,7 +57,7 @@ const Partners = () => {
             enableAutoPlay={true}
             autoPlaySpeed={3000}
             isRTL
-            itemsToShow={4}
+            itemsToShow={handleCarousel()}
             transitionMs={1000}
             tiltEasing="ease"
             enableTilt
