10fa4a9642b6673c5e07ad3b59da4bbaa1774fdb
Igor Ribeiro <igorflribeiro@gmail.com>
2021-04-16 19:24:59 -0300

    fix: baseurl corrected

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://devrental-api.herokuapp.com/",
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://devrental-api.herokuapp.com/",
 });
 export default api;
