a4a73b650b151d31dcd2ad39e85f777115ccb35c
Igor Ribeiro <igorflribeiro@gmail.com>
2021-04-17 19:00:01 -0300

    feature: component added for the currentchallenge tab on the dashboard

--- src/Provider/DataMap/index.tsx
+++ src/Provider/DataMap/index.tsx
@@ -22,15 +22,22 @@ interface IdataState {
 interface IcontextData {
   itemMap: IdataState[];
   setItemMap: React.Dispatch<React.SetStateAction<IdataState[]>>;
+  currentWindow: string;
+  setCurrentWindow: React.Dispatch<React.SetStateAction<string>>;
+  currentJob: any;
+  setCurrentJob: any;
+
 }
 
 export const DataMapContext = createContext<IcontextData>({} as IcontextData);
 
 export const DataMapProvider = ({ children }: IdataChildren) => {
   const [itemMap, setItemMap] = useState<IdataState[]>([]);
+  const [currentWindow, setCurrentWindow] = useState<string>('Desafios')
+  const [currentJob, setCurrentJob] = useState<any>({})
 
   return (
-    <DataMapContext.Provider value={{ itemMap, setItemMap }}>
+    <DataMapContext.Provider value={{ itemMap, setItemMap, currentWindow, setCurrentWindow, currentJob, setCurrentJob }}>
       {children}
     </DataMapContext.Provider>
   );
