283ee048ed3d40c3c19dbb712180f4a1d1ae5b56
Igor Ribeiro <igorflribeiro@gmail.com>
2021-04-18 17:27:11 -0300

    feature: portfolio now renders jobs previously

--- src/components/PortfolioData/index.tsx
+++ src/components/PortfolioData/index.tsx
@@ -1,21 +1,47 @@
-import { Container } from "./style";
+// @ts-nocheck
 
-interface IAceptedWork {
-  title: string;
-  objective: string;
-  description: string;
-  tecnology: string[];
-  reward: string;
-  workDeadline: string;
-}
-interface IItem {
-  item: IAceptedWork;
-}
+import { Container, Card, List } from "./style";
+import {useEffect, useState} from "react";
+import api from "../../services";
 
-const PortfolioData = ({ item }: IItem) => {
-  console.log("gatos: ", item);
+const PortfolioData = () => {
 
-  return <Container>gsdghdsghdsghsd</Container>;
+  const idUser = JSON.parse(localStorage.getItem("userId") ?? "");
+  let user = JSON.parse(localStorage.getItem("token") ?? "");
+
+  const [a, setA] = useState<any>([])
+
+  useEffect(()=>{
+    async function fetchData(){
+    const data = await api.get(`portfolio/?userId_like=${idUser}`, {
+    headers: { Authorization: `Bearer ${user}` }})
+    console.log('portfolio', data)
+    setA(data.data)
+
+    return null
+
+    }
+    fetchData()
+
+  },[])
+
+  return <Container>
+      <h2>Projetos passados</h2>
+
+    {console.log(a)}
+    <List>
+      {a.map((element, index) =>
+      <Card key={index}>
+        <div>
+
+          <h3><span>Título:</span> {element.item.title}</h3>
+          <h3><span>Objetivo:</span> {element.item.objective}</h3>
+          <h3><span>Link do deploy:</span>{element.text}</h3>
+
+        </div>
+      </Card>)}
+    </List>
+  </Container>;
 };
 
 export default PortfolioData;
--- src/components/PortfolioData/style.ts
+++ src/components/PortfolioData/style.ts
@@ -1,3 +1,35 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    h2{
+      font-size: 2rem;
+      color: #fff;
+      margin-bottom: 2rem;
+    }
+`;
+
+export const List = styled.ul`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+export const Card = styled.li`
+    color: #fff;
+    margin-bottom: 2rem;
+  
+  h3{
+    margin-bottom: 0.2rem;
+    span{
+      color: #ccc;
+      margin-right: 1rem;
+    }
+    margin-bottom: 0.5rem;
+  }
+  
+  h3:first-child{
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+  }
+  
+`;
